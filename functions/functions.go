3fd15c490e1910489e16e132896c6752a54e618d
mjgoerlitz-new <mjgoerlitz@icloud.com>
2024-01-07 22:28:42 -0800

    function work

--- functions/functions.go
+++ functions/functions.go
@@ -1,5 +1,38 @@
 package functions
 
-func PracticeFunction() {
+import "fmt"
+
+//	func sub(x int, y int) int {
+//		return x - y
+//	}
+func HelloWorld() string {
+	return "hello world"
+}
+
+func Subtract(x int, y int) int {
+	return x - y
+}
+
+func Addition(x, y int, template string) string {
+	sum := x + y
+	return template + fmt.Sprintf("%d", sum)
+}
+
+func IncrementNoReturn(x int) {
+	x++
+}
+
+func IncrementReturn(x int) int {
+	x++
+	return x
+}
+
+func TestIncrements() {
+	x := 5
+	IncrementNoReturn(x)
+	fmt.Println(x) // should still be 5
+
+	x = IncrementReturn(x)
+	fmt.Println(x) // should be 6
 
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/mjgoerlitz-new/go-demo-1/functions"
 	"github.com/mjgoerlitz-new/go-demo-1/helloworld"
 	"github.com/mjgoerlitz-new/go-demo-1/variables"
 )
@@ -10,4 +11,8 @@ import (
 func main() {
 	fmt.Println(helloworld.HelloWorld())
 	variables.SetVariables()
+	fmt.Println("\n", functions.HelloWorld())
+	fmt.Println(functions.Subtract(2, 1))
+	fmt.Println(functions.Addition(1, 2, "The sum is "))
+	functions.TestIncrements()
 }
